a62c4d4656262f419ed7d71ca0f00ce232d063a2
2xroygbiv7 <patrick@theonevehicle.com>
2022-03-05 03:58:00 +0000

    added navbar & messages partials

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,7 @@
 class StaticPagesController < ApplicationController
   def landing_page
+    #flash.now[:notice] = "Sample flash notice message."
+    #flash.now[:alert] = "Sample flash alert message."
   end
+
 end
