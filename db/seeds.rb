aa23b15cc91f8e68015f08cadefc9ca6b56431ff
2xroygbiv7 <patrick@theonevehicle.com>
2022-03-06 02:35:48 +0000

    added image to product index

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,11 +2,12 @@ Category.create!([{
     name: "Energel"
   }])
   
-for i in 1..5
+for i in 1..8
   Product.create!([{
     code: "BL00#{i}",
     name: "Energel BL00#{i}",
-    category_id: Category.first.id
+    category_id: Category.first.id,
+    image: "https://placehold.co/330x330?text=BL00#{i}"
   }])
 end
 
